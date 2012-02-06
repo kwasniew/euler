793b5668bcab1d61c32bb70a3c673e95f77d531c
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2012-02-06 16:12:33 +0100

    improved performance

--- euler002.hs
+++ euler002.hs
@@ -1,5 +1,7 @@
 fib 0 = 1
 fib 1 = 2
-fib x = fib (x - 2) + fib (x - 1)
+fib x = fibMemo (x - 2) + fibMemo (x - 1)
+
+fibMemo = (map fib [0..] !!)
 
 main = print $ sum $ filter even $ takeWhile (<=4000000) $ map fib [1..]
